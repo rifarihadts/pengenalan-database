843773c2aa71e544ba7fe9fc6e4fa451ff28b25a
rifarihadts <rifariha.dts@gmail.com>
2020-10-21 17:35:44 +0700

    pengenalan database

---
+++ sql-orm/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/FadhlanHawali/Digitalent-Kominfo_Introduction-Database-1/sql-generic/config"
+	"github.com/FadhlanHawali/Digitalent-Kominfo_Introduction-Database-1/sql-orm/database"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	cfg, err := getConfig()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	db, err := initDB(cfg.Database)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	database.InsertCustomer(database.CustomerORM{
+		FirstName:    "Ridho",
+		LastName:     "Fariha",
+		NpwpId:       "id-1",
+		Age:          10,
+		CustomerType: "Standart",
+		Street:       "Jalan Garu 1",
+		City:         "Medan",
+		State:        "Indonesia",
+		ZipCode:      "20117",
+		PhoneNumber:  "081238445413",
+	}, db)
+
+	database.GetCustomers(db)
+	//database.DeleteCustomer(1,db)
+	//database.UpdateCustomer(database.CustomerORM{PhoneNumber: "0812314"},2,db)
+	//
+	database.InsertAccount(database.AccountORM{
+		Balance:     50000,
+		AccountType: "Deposit",
+	}, 2, db)
+}
+
+func getConfig() (config.Config, error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config.yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return config.Config{}, err
+	}
+
+	var cfg config.Config
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func initDB(dbConfig config.Database) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.DbName, dbConfig.Config)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	db.AutoMigrate(
+		&database.CustomerORM{},
+		&database.AccountORM{},
+	)
+
+	log.Println("db successfully connected")
+
+	return db, nil
+}
